c4fb45aa22bbc513ecb32cfbbe4df101b86686b1
HirokiKosaka <132083053+HirokiKosaka@users.noreply.github.com>
2024-05-25 22:05:49 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotal } from "./features/cart/CartSlice";
+
+
+function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+  useEffect(() => {
+    dispatch(calculateTotal())
+  }, [cartItems]);
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
